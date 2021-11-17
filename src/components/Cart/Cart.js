9406666377167734ab3a5aa55c3e52facc7f0c83
codu2 <82283517+codu2@users.noreply.github.com>
2021-11-17 17:43:03 +0900

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,49 @@
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = props => {
-    const cartitems = <ul className={classes['cart-items']}>{[{id: 'c1', name:'Sushi', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    console.log(totalAmount)
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
+
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 });
+        //추측이지만 아마 카트에서 + 버튼을 누를 때마다 amount가 한 개씩 증가해서 amount: 1 넣은 듯?
+    };
+
+    const cartitems = <ul className={classes['cart-items']}>
+            {cartCtx.items.map((item) => (
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    amount={item.amount} 
+                    price={item.price} 
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)} 
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
+            ))}
+        </ul>;
 
     return (
         <Modal onClose={props.onClose}>
             {cartitems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+                {hasItems && <button className={classes.button}>Order</button>}
             </div>
         </Modal>
     );
@@ -22,3 +52,4 @@ const Cart = props => {
 export default Cart;
 
 //cartitems, total amount, cart actions(close and order button)
+//bind()함수
