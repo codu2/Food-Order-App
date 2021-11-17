3e93137aedcc4128490caff55b84d849ba4c7f4c
codu2 <82283517+codu2@users.noreply.github.com>
2021-11-17 17:47:41 +0900

    Update Input.js

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,15 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = props => {
-    return <div className={classes.input}>
-        <label htmlFor={props.input.id}>{props.label}</label>
-        <input {...props.input}/>
-    </div>
-};
+const Input = React.forwardRef((props, ref) => {
+    return (
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input ref={ref} {...props.input}/>
+        </div>
+    );
+});
 
 export default Input;
 
@@ -13,3 +17,5 @@ export default Input;
 //예를 들어 <Input label="Amount" input={{type: 'number', id:'amount'}}이면
 //실제로는 <input type="number" id="amount">가 되는 것이다. 
 //Input component를 재사용하면서도 밖에서 값을 조정할 수 있도록 하기 위함이다.
+
+//ref로 MealItemForm에서 amountInputRef를 전달받고 이를 <input />태그에 ref={ref}로 다시 전달받음
